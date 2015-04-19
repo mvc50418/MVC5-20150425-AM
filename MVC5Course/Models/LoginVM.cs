4d44b7fa8c34dffbdd9da380763dd8cc96d65273
Will Huang <doggy.huang@gmail.com>
2015-04-19 13:05:11 +0800

    設定 LoginVM 的 Password 的 DataType 屬性，讓欄位可以在輸入密碼時做隱碼處裡

--- MVC5Course/Models/LoginVM.cs
+++ MVC5Course/Models/LoginVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace MVC5Course.Models
     public class LoginVM
     {
         public string Username { get; set; }
+
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
